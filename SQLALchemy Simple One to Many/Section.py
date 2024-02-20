fb70de3087789aee22b86f9af5669188a7b4e90f
Will-Trinh <william.k.trinh@gmail.com>
2024-02-19 16:42:54 -0800

    Added Section.py Imports

--- SQLALchemy Simple One to Many/Section.py
+++ SQLALchemy Simple One to Many/Section.py
@@ -2,9 +2,8 @@ from orm_base import Base
 from db_connection import engine
 from IntrospectionFactory import IntrospectionFactory
 from sqlalchemy import UniqueConstraint, ForeignKeyConstraint
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Time
 from sqlalchemy.orm import Mapped, mapped_column, relationship, column_property
 from sqlalchemy import Table
-from Department import Department
 from Course import Course
 from constants import START_OVER, REUSE_NO_INTROSPECTION, INTROSPECT_TABLES
