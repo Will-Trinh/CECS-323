9cbaa65635902adcac5e1f1f4b0fdb15778f3bcd
030080022 <ellie.holt01@student.csulb.edu>
2024-02-19 19:08:46 -0800

    changes to departmentClass

--- SQLALchemy Simple One to Many/DepartmentClass.py
+++ SQLALchemy Simple One to Many/DepartmentClass.py
@@ -1,19 +1,32 @@
 from sqlalchemy import UniqueConstraint
 from sqlalchemy import String
+from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List  # Use this for the list of courses offered by the department
 
 """This is the guts of the department class that needs to be defined
 regardless whether we introspect or not."""
 __tablename__ = "departments"  # Give SQLAlchemy th name of the table.
-abbreviation: Mapped[str] = mapped_column('abbreviation', String,
-                                          nullable=False, primary_key=True)
+
+abbreviation: Mapped[str] = mapped_column('abbreviation', String, nullable=False, primary_key=True)
+
+#added
+name: Mapped[str] = mapped_column('name', String(50), nullable = False, primary_key = True)
+chairName: Mapped[str] = mapped_column("chair_name", String(80), nullable=False)
+building: Mapped[str] = mapped_column("building", String(10), nullable=False)
+office: Mapped[int] = mapped_column("office", Integer, nullable=False)
+description: Mapped[str] = mapped_column("description", String(80), nullable=False)
+##
+
 courses: Mapped[List["Course"]] = relationship(back_populates="department")
 name: Mapped[str] = mapped_column('name', String(50), nullable=False)
 # __table_args__ can best be viewed as directives that we ask SQLAlchemy to
 # send to the database.  In this case, that we want two separate uniqueness
 # constraints (candidate keys).
-__table_args__ = (UniqueConstraint("name", name="departments_uk_01"),)
+__table_args__ = (UniqueConstraint("name", name="departments_uk_01"),
+                    UniqueConstraint("chair_name", name="departments_uk_02"),
+                    UniqueConstraint("building", "office", name="departments_uk_03"),
+                    UniqueConstraint("description", name="departments_uk_04"))
 
 
 def add_course(self, course):
@@ -31,4 +44,4 @@ def get_courses(self):
 
 
 def __str__(self):
-    return f"Department abbreviation: {self.abbreviation} name: {self.name} number course offered: {len(self.courses)}"
+    return f"Department abbreviation: {self.abbreviation} name: {self.name} \n Location: {self.building} {self.office} \n number course offered: {len(self.courses)}"
