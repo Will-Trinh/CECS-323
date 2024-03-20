b8acc0c077869de040ae2bad014cc1d463108e07
Will-Trinh <william.k.trinh@gmail.com>
2024-03-19 19:39:08 -0700

    Updated __main__

--- MongoDB Single Collection/main.py
+++ MongoDB Single Collection/main.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
     print(f"Students in the collection so far: {student_count}")
 
     departments = db["departments"]
-    departments_count = deparments.count_documents({})
+    departments_count = departments.count_documents({})
     print(f"Departments in the collection so far: {student_count}")
 
     create_students_collection(students)
