8c2d22cc8951e65333ca41bd62dca961caebc2b4
spizytoku <89183831+spizytoku@users.noreply.github.com>
2024-04-07 21:06:51 -0700

    updated PriceHistory.py

--- MongoDB One to Many/PriceHistory.py
+++ MongoDB One to Many/PriceHistory.py
@@ -7,10 +7,10 @@ class PriceHistory(EmbeddedDocument):
     price = Decimal128Field(db_field='new_price', min_value=0.01, precision=2, required=True)
     priceChangeDate = DateTimeField(db_field='price_change_date', required=True)
 
-    def __init__(self, price: Decimal, price_change_date: datetime, *args, **kwargs):
+    def __init__(self, price: Decimal, priceChangeDate: datetime, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.price = price
-        self.priceChangeDate = price_change_date
+        self.priceChangeDate = priceChangeDate
 
     def __str__(self):
         return (f'New Price: {self.price} \n'
