07142062ea3fd3a7aae1f169078ffd1a449d035c
spizytoku <89183831+spizytoku@users.noreply.github.com>
2024-04-07 20:46:25 -0700

    Added menu options

--- MongoDB One to Many/menu_definitions.py
+++ MongoDB One to Many/menu_definitions.py
@@ -28,6 +28,7 @@ add_select = Menu('add select', 'Which type of object do you want to add?:', [
 # options for deleting an existing instance
 delete_select = Menu('delete select', 'Which type of object do you want to delete?:', [
     Option("Orders", "delete_order()"),
+    Option("Products", "delete_product()"),
     Option("Order Items", "delete_order_item()"),
     Option("Exit", "pass")
 ])
@@ -42,6 +43,7 @@ list_select = Menu('list select', 'Which type of object do you want to list?:', 
 # options for testing the select functions
 select_select = Menu('select select', 'Which type of object do you want to select:', [
     Option("Order", "print(select_order())"),
+    Option("Products", "print(select_product())"),
     Option("Order Item", "print(select_order_item())"),
     Option("Exit", "pass")
 ])
@@ -49,6 +51,7 @@ select_select = Menu('select select', 'Which type of object do you want to selec
 # options for testing the update functions
 update_select = Menu("update select", 'Which type of object do you want to update:', [
     Option("Order", "update_order()"),
+    Option("Products", "change_product()"),
     Option("Order Items", "update_order_item"),
     Option("Exit", "pass")
 ])
