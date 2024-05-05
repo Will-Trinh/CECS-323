66efe899d165e9abe93d6cb5d7e14a7ae052dd34
LehoPan <leo.long.pan@gmail.com>
2024-05-05 16:17:30 -0700

    Fleshed out departments

--- MongoDB Enrollment Project/Departments.py
+++ MongoDB Enrollment Project/Departments.py
@@ -2,15 +2,29 @@ from mongoengine import *
 
 class Department(Document):
 
-    customerName = StringField(db_field='customer_name', max_length=80, min_length=5, required=True)
+    name = StringField(db_field='name', max_length=80, min_length=1, required=True)
+    abbreviation = StringField(db_field='abbreviation', max_length=6, min_length=1, required=True)
+    chairName = StringField(db_field='chair_name', max_length=80, min_length=1, required=True)
+    building = StringField(db_field='building', required=True)
+    office = IntField(db_field='office', min_value=1, required=True, choices=('ANAC', 'CDC', 'DC', 'ECS', 'EN2', 'EN3', 'EN4', 'EN5', 'ET', 'HSCI', 'NUR', 'VEC'))
+    description = StringField(db_field='description', max_length=80, min_length=1, required=True)
 
     meta = {'collection': 'departments',
             'indexes': [
-                {'unique': True, 'fields': ['customerName', 'orderDate'], 'name': 'orders_pk'}
+                {'unique': True, 'fields': ['name'], 'name': 'departments_uk_01'},
+                {'unique': True, 'fields': ['abbreviation'], 'name': 'departments_uk_02'},
+                {'unique': True, 'fields': ['chair_name'], 'name': 'departmnets_uk_03'},
+                {'unique': True, 'fields': ['building, office'], 'name': 'departments_uk_04'}
             ]}
     
-    def __init__(self):
-        pass
+    def __init__(self, name: str, abbreviation: str, chairName: str, building: str, office: int, description: str, *args, **values):
+        super().__init__(*args, **values)
+        self.name = name
+        self.abbreviation = abbreviation
+        self.chairName = chairName
+        self.building = building
+        self.office = office
+        self.description = description
 
     def __str__(self):
-        pass
+         return f'Department: name: {self.name}, Abbreviation: {self.abbreviation}, Chair: {self.chairName}, Location: {self.building} {str(self.office)}, Description: {self.description}'
