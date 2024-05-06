16d524ac0c89388957e02c9f052fb551528db642
Ellie816 <elinor.holt1@gmail.com>
2024-05-05 18:53:49 -0700

    Update PassFails.py

--- MongoDB Enrollment Project/PassFails.py
+++ MongoDB Enrollment Project/PassFails.py
@@ -1,16 +1,24 @@
 from mongoengine import *
+from datetime import *
 
 class PassFail(Document):
 
-    customerName = StringField(db_field='customer_name', max_length=80, min_length=5, required=True)
+    enrollment = ObjectIdField(db_field='enrollment', required=True)
+    applicationDate = DateField(db_field='application_date', min_value= datetime.now, required=True)
+
+
+    meta = {'collection': 'passFails',
+            'indexes': [
+                {'unique': True, 'fields': ['enrollment'], 'name': 'passFails_uk_01'}
+            ]}
     
-    # meta = {'collection': 'pass_fails',
-    #         'indexes': [
-    #             {'unique': True, 'fields': ['customerName', 'orderDate'], 'name': 'orders_pk'}
-    #         ]}
-    
-    def __init__(self):
-        pass
+    def __init__(self, enrollment, applicationDate: date, *args, **values):
+        super().__init__(*args, **values)
+        self.enrollment = enrollment
+        self.applicationDate = applicationDate
+
 
     def __str__(self):
-        pass
+        return (f'Enrollment ID: {self.enrollment}\n'
+                f'Application Date: {self.applicationDate}')
+    
