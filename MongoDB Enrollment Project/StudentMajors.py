a0e0ac4478dc758b8b60b3269994660de7e50047
spizytoku <89183831+spizytoku@users.noreply.github.com>
2024-05-05 17:25:28 -0700

    updated StudentMajors.py.py

--- MongoDB Enrollment Project/StudentMajors.py
+++ MongoDB Enrollment Project/StudentMajors.py
@@ -1,16 +1,31 @@
 from mongoengine import *
+import mongoengine
+from Majors import Major
+from Students import Student
+from datetime import datetime
 
 class StudentMajor(Document):
 
-    customerName = StringField(db_field='customer_name', max_length=80, min_length=5, required=True)
-    
+    student = StringField(db_field='student', max_length=80, min_length=1, required=True)
+    major = StringField(db_field='major', max_length=80, min_length=1, required=True)
+    studentId = StringField(db_field='student_id', max_length=80, min_length=1, required=True)
+    majorName = StringField(db_field='major_name', max_length=80, min_length=1, required=True)
+    declarationDate = DateTimeField(db_field='declarationDate',
+                                    min_value=datetime.datetime.combine(datetime.date.min, datetime.time(0, 0)),
+                                    max_value=datetime.datetime.now(),
+                                    required=True)
     meta = {'collection': 'student_majors',
             'indexes': [
-                {'unique': True, 'fields': ['customerName', 'orderDate'], 'name': 'orders_pk'}
+                {'unique': True, 'fields': ['student', 'majorName'], 'name': 'studentMajors_uk_01'}
             ]}
-    
-    def __init__(self):
-        pass
+
+    def __init__(self, student: str, major: str, studentId: str, majorName: str, declarationDate: datetime, *args, **values):
+        super().__init__(*args, **values)
+        self.student = student
+        self.major = major
+        self.studentId = studentId
+        self.majorName = majorName
+        self.declarationDate = declarationDate
 
     def __str__(self):
-        pass
+        return f"Student major - student: {self.student} major: {self.major}"
